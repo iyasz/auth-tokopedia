984862682100382fad77fee054c38471f30482ae
Yasz Avellia <zakamaragames@gmail.com>
2022-10-05 20:01:13 +0700

    menambah input type form

---
+++ login.php
@@ -0,0 +1,46 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row justify-content-center ">
+            <div class="col-lg-5">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <div class="login-box">
+                                <div class="user-box">
+                                    <input type="text" class="form-control" name="">
+                                    <label for="">Username</label>
+                                </div>
+                                <div class="user-box">
+                                    <input type="text" name="" class="form-control">
+                                    <label for="">Password</label>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+
+</html>
