cccfdd187a4ca9ea6691532ba8fbb8ee32044268
Yasz Avellia <zakamaragames@gmail.com>
2022-10-29 14:28:11 +0700

    menambah bootstrap template

--- login.php
+++ login.php
@@ -16,28 +16,9 @@
 </head>
 
 <body>
-    <div class="container">
-        <div class="row justify-content-center ">
-            <div class="col-lg-5">
-                <div class="card shadow">
-                    <div class="card-body">
-                        <form action="" method="post">
-                            <div class="login-box">
-                                <div class="user-box">
-                                    <input type="text" class="form-control" name="">
-                                    <label for="">Username</label>
-                                </div>
-                                <div class="user-box">
-                                    <input type="text" name="" class="form-control">
-                                    <label for="">Password</label>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
+
+
     <!-- bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
