abfa97c4f9efe664fd2879a9f37fadf9e39ce0d2
Yasz Avellia <zakamaragames@gmail.com>
2022-10-31 17:24:50 +0700

    menambah auth

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
-<?php 
+<?php
 
 include "layouts/header.php";
+
 include "app/views.php";
+
 include "layouts/footer.php";
---
+++ layouts/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+
+    <!-- css  -->
+    <link rel="stylesheet" href="assets/css/costum.css">
+    <link rel="stylesheet" href="assets/css/auth.css">
+
+    <title>Login </title>
+</head>
+
+<style>
+    @media only screen and (min-width: 769px) {
+        body {
+            background-image: url("assets/img/bg.png");
+            background-size: 790px;
+            background-repeat: no-repeat;
+            background-position: center;
+            background-attachment: fixed;
+            background-position-y: 130px;
+        }
+
+        .card-lg {
+            width: 82%;
+            margin-left: 10%;
+        }
+    }
+</style>
+
+<body>
