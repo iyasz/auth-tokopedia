086e55024df442c68567f96aa806c7a8d9ce9150
Yasz Avellia <zakamaragames@gmail.com>
2022-10-31 17:44:23 +0700

    menambah font

--- layouts/header.php
+++ layouts/header.php
@@ -8,9 +8,13 @@
     <!-- bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
 
+    <!-- font  -->
+    <link rel="stylesheet" media="screen" href="https://fontlibrary.org//face/open-sauce-one" type="text/css" />
+
     <!-- css  -->
     <link rel="stylesheet" href="assets/css/costum.css">
     <link rel="stylesheet" href="assets/css/auth.css">
+    <link rel="stylesheet" href="assets/css/font.css">
 
     <title>Login </title>
 </head>
