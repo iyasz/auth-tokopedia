a5cd0675ef4c4aa1f72412120ae34078e6c912ea
Yasz Avellia <zakamaragames@gmail.com>
2022-11-02 09:21:45 +0700

    menambah folder dan file js

--- views/auth/register.php
+++ views/auth/register.php
@@ -1,19 +1,35 @@
-<div class="container">
-    <div class="row">
-        <div class="col-lg-6 d-lg-block d-md-block d-none">
+<div class="container ">
+    <div class="row mt-4">
+        <div class="col-lg-6 d-lg-block d-md-block d-none mt-5">
             <div class="text-center ff-open ">
-                <img src="assets/img/register_new.png" width="400px" alt="">
-                <h3 class="fw-bold">Jual Beli Hudah Hanya di Tokopedia</h3>
-                <p>Gabung dan rasakan kemudahan bertransaksi di Tokopedia</p>
+                <img src="assets/img/register_new.png" width="370px" alt="">
+                <h4 class="mt-4 mb-3 fw-bold fs">Jual Beli Hudah Hanya di Tokopedia</h4>
+                <p class="fs-log2 fw-semibold opacity-75 letter-login3">Gabung dan rasakan kemudahan bertransaksi di Tokopedia</p>
             </div>
         </div>
         <div class="col-lg-5 col-md-6 col-sm-12">
-            <div class="card card-login shadow-style border-0">
-                <div class="header text-center ">
-                    <h5 class="ff-open fw-bold">Daftar Sekarang</h5>
-                    <p>Sudah punya akun Tokopedia? <a href="">Masuk</a></p>
+            <div class="card card-login shadow-style2 border-0">
+                <div class="card-body mx-4">
+                    <div class="header text-center ">
+                        <h4 class="text-blackToGray mb-1 ff-open  fw-bold">Daftar Sekarang</h4>
+                        <p class="fs-log opacity-75 letter-login ">Sudah punya akun Tokopedia? <a class="text-decoration-none textLinkGreen" href="index.php?page=login">Masuk</a></p>
+                        <a class="btn btn-primary w-100 bg-transparent allMethod google  fw-semibold mt-2 " href=""><img src="assets/img/googleicon.svg" width="18px" class="me-4"> Google</a>
+                    </div>
+                    <div class="text-gray d-flex align-items-center lnHR mb-1">
+                        <span class="lineHorizontal"></span>
+                        <p class="fw-semibold text-center w-50 ff-open fs-log2 letter-login mx-lg-3 mx-md-0 mx-0">atau masuk dengan</p>
+                        <span class="lineHorizontal"></span>
+                    </div>
+                    <div class="">
+                        <p class="mb-1 fs-log3 fw-semibold opacity-50 ff-open">Phone Number or Email</p>
+                        <input type="text" class="form-control input-login">
+                        <p class="fs-log2 letter-login mt-1 text-gray fw-semibold">Example: email@tokopedia.com</p>
+                        <button type="submit" class="btn btn-primary mt-2 w-100 letter-login2 fw-bold button-login py-2 rounded-3">Daftar</button>
+                        <div class="text-center opacity-75 mt-3">
+                            <p class="ff-open fs-log2 letter-login">Dengan mendaftar, saya menyetujui <br> <a href="" class="text-decoration-none textLinkGreen">Syarat dan Ketentuan</a> serta <a href="" class="text-decoration-none textLinkGreen">Kebijakan Privasi</a></p>
+                        </div>
+                    </div>
                 </div>
-
             </div>
         </div>
     </div>
